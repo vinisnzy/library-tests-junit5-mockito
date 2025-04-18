b2b9a3be9b0ecd523689e483e1e5e7b24581e763
Vinícius de Paula <manoseilacara123@gmail.com>
2025-04-18 15:09:42 -0300

    feat: initial config

---
+++ src/main/java/com/vinisnzy/api_library_tests/ApiLibraryTestsApplication.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.api_library_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLibraryTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLibraryTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinisnzy/api_library_tests/ApiLibraryTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisnzy.api_library_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLibraryTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
